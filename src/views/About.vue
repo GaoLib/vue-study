35f3fb7a11522308ea5537ef0cdeba536dc31915
Gao <xingaoemail@163.com>
2021-03-04 10:29:21 +0800

    add: basic files

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <span @click="add('4', '5')">{{ number }}</span>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 1
+    }
+  },
+  methods: {
+    add(num1, num2) {
+      this.number = this.number + num1 + num2
+    }
+  },
+}
+</script>
